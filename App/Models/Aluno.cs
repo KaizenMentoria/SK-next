c9d58107d9f16e59e2919d4385e30f2f29c45ba1
Lucas Gabriel Castro <lucasgabrielcastro@outlook.com>
2024-05-27 19:48:30 +0000

    Entidade aluno feita

--- App/Models/Aluno.cs
+++ App/Models/Aluno.cs
@@ -20,10 +20,14 @@ public class Aluno
     public string Sobrenome { get; set; }
 
     [RegularExpression("[0-9]{2}-[0-9]{5}-[0-9]{4}", ErrorMessage = "Número de contato tem de estar no formato (00) 00000-0000")]
-    [Required]
-    [Display(Name = "Número de Contato")]
-    public string Contato { get; set; }
+    [Required(ErrorMessage = "Número de telefone é obrigatório.")]
+    [Display(Name = "Número de Telefone")]
+    public string NumeroTelefone { get; set; }
+
+    [EmailAddress]
+    [Required(ErrorMessage = "Email é obrigatório")]
     
     [Display(Name = "Data de Nascimento")]
+    [DataType(DataType.DateTime)]
     public DateTime Nascimento { get; set; }
 }
