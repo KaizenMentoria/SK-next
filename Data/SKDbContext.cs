69e744f324b3adaa21744b7d300177e0a15051e1
matheusanmo <andrade94kaizen@gmail.com>
2024-05-27 22:09:18 +0000

    alunos e turmas no dbctx

--- Data/SKDbContext.cs
+++ Data/SKDbContext.cs
@@ -1,13 +1,29 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace workspace.Data
 {
+    using workspace.Models;
     public class SKDbContext : DbContext
     {
         public SKDbContext(DbContextOptions<SKDbContext> options)
-            : base(options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder
+                .Entity<Aluno>()
+                .Property(a => a.CreationTimestamp)
+                .HasDefaultValueSql("now()");
+            modelBuilder
+                .Entity<Turma>()
+                .Property(a => a.CreationTimestamp)
+                .HasDefaultValueSql("now()");
         }
+
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Turma> Turmas { get; set; }
+
     }
 }
