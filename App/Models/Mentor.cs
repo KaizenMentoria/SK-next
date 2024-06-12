f5a136c31d436203d8a861da66860482081f0b09
Lucas Gabriel Castro <lucasgabrielcastro@outlook.com>
2024-06-12 22:45:16 +0000

    Model, Views e Controller criados (porém não implementados)

---
+++ App/Models/Mentor.cs
@@ -0,0 +1,4 @@
+// TODO: Implementar a classe Mentor (modelo)
+// Lembrar: 
+// - Implementar a relação one-to-many com a classe Aluno
+// - Fazer atualização no banco de dados, por meio de Migration no Entity Framework
