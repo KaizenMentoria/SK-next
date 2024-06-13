3dda8124c3aab1c087881d2c287e9e38496c4334
Lucas Gabriel Castro <lucasgabrielcastro@outlook.com>
2024-06-13 16:43:26 +0000

    Mentor: Controller criado + Método Index e Create

--- App/Controllers/MentorController.cs
+++ App/Controllers/MentorController.cs
@@ -1,1 +1,45 @@
 // TODO: implementar CRUD de mentores
+using Microsoft.AspNetCore.Mvc;
+using App.Models;
+using App.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Controllers;
+
+public class MentorController : Controller
+{
+    private readonly ILogger<MentorController> _logger;  
+    private readonly ApplicationDbContext _dbContext;
+
+    public MentorController(ILogger<MentorController> logger, ApplicationDbContext dbContext)
+    {
+        _logger = logger;
+        _dbContext = dbContext;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        return View(await _dbContext.Mentores.ToListAsync());
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Mentor novoMentor)
+    {
+        novoMentor.DataNascimento = DateTime.SpecifyKind(novoMentor.DataNascimento, DateTimeKind.Utc);
+
+        if (!ModelState.IsValid)
+        {
+            return View(novoMentor);
+        }
+
+        _dbContext.Add(novoMentor);
+        await _dbContext.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+}
