acb1bb52a8ba3767933babd6233b6e25c1bd5be7
Lucas Gabriel Castro <lucasgabrielcastro@outlook.com>
2024-06-13 16:53:35 +0000

    Mentor entity class created

--- App/Models/Mentor.cs
+++ App/Models/Mentor.cs
@@ -2,3 +2,40 @@
 // Lembrar: 
 // - Implementar a relação one-to-many com a classe Aluno
 // - Fazer atualização no banco de dados, por meio de Migration no Entity Framework
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace App.Models;
+
+public class Mentor
+{
+    [Required]
+    public int MentorId { get; set; }
+
+    [Required(ErrorMessage = "Primeiro nome é obrigatório")]
+    [Display(Name = "Primeiro Nome")]
+    public string PrimeiroNome { get; set; }
+    
+    [Display(Name = "Segundo Nome")]
+    public string? SegundoNome { get; set; }
+
+    [Required(ErrorMessage = "Sobrenome é obrigatório.")]
+    [Display(Name = "Sobrenome")]
+    public string Sobrenome { get; set; }
+
+    [RegularExpression("[0-9]{2}-[0-9]{5}-[0-9]{4}", ErrorMessage = "Número de contato tem de estar no formato (00) 00000-0000")]
+    [Required(ErrorMessage = "Número de telefone é obrigatório.")]
+    [Display(Name = "Número de Telefone")]
+    public string NumeroTelefone { get; set; }
+
+    [EmailAddress]
+    [Required(ErrorMessage = "Email é obrigatório")]
+    public string Email { get; set; }
+    
+    [Display(Name = "Data de Nascimento")]
+    [DataType(DataType.DateTime)]
+    public DateTime DataNascimento { get; set; }
+
+    public ICollection<Aluno> Alunos { get; set; } = new List<Aluno>();
+}
